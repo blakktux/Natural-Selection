5bb3df36bc05d797418fea45e4bca76538888267
Zachary Maciejewski <zachmaczbm@gmail.com>
2015-10-16 13:12:02 -0400

    Change Resources

--- src/main/java/edu/gatech/mulegame/gui/application/RandomEventHandler.java
+++ src/main/java/edu/gatech/mulegame/gui/application/RandomEventHandler.java
@@ -11,9 +11,9 @@ public class RandomEventHandler {
     public static void eventHandler(Random rand, Player p, boolean lowest) {
         int eventNum = 0;
         if (lowest) {
-            eventNum = rand.nextInt(5) ;
+            eventNum = rand.nextInt(10) ;
         } else {
-            eventNum = rand.nextInt(10);
+            eventNum = rand.nextInt(15);
         }
 
         //print statement if lowest
@@ -29,14 +29,24 @@ public class RandomEventHandler {
         } else if (eventNum == 4) {
             p.changeMoney(500);
         } else if (eventNum == 5) {
-            p.changeMoney(0);
+            p.changeResource(0, 50);
         } else if (eventNum == 6) {
-            p.changeMoney(-50);
+            p.changeResource(0, 100);
         } else if (eventNum == 7) {
-            p.changeMoney(-100);
+            p.changeResource(1, 50);
         } else if (eventNum == 8) {
-            p.changeMoney(-200);
+            p.changeResource(1, 100);
         } else if (eventNum == 9) {
+            p.changeResource(2, 50);
+        }else if (eventNum == 10) {
+            p.changeResource(2, 100);
+        } else if (eventNum == 11) {
+            p.changeMoney(-50);
+        } else if (eventNum == 12) {
+            p.changeMoney(-100);
+        } else if (eventNum == 13) {
+            p.changeMoney(-200);
+        } else if (eventNum == 14) {
             p.changeMoney(-500);
         }
 
